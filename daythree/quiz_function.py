6366d13599c020977be67795429f396f83cd8cf9
adedotun <erik.edwards.jon@gmail.com>
2025-10-29 10:57:21 -0700

    latest

--- daythree/quiz_function.py
+++ daythree/quiz_function.py
@@ -1,25 +1,28 @@
 import random
-questions=["what is my name?", "who is your java sensei?","whats the building you are in called?","what is the name of the guy from chad"]
+questions=["what is my name?", "who is your java sensei?","whats the building you are in called?","what is the name of the guy from chad","Which planet is known as the Red Planet?","Who painted the Mona Lisa?","Which element has the chemical symbol 'O'?","Which country invented paper?"]
 
-answers=["adedotun , miss ope , nisi" , "benny,miss ope ,beny,lagos,SK","ebuka,lagos,sabo,semi-colon-village","SK,class daddy,nissi,beny"]
+answers=["adedotun","miss ope","village","beny","mars","Leonardo","Oxygen","China","lagos","ebuka","lagos","sabo","semi-colon","SK","class daddy","nissi","moses","ekuwe","nitrogen","earth","jupiter","femi","korean"]
 
-validate_answer = {"what is my name?":"adedotun","who is your java sensei?": "miss ope" , "whats the building you are in called?":"village", "what is the name of the guy from chad":"beny"}
+validate_answer = {"what is my name?":"adedotun","who is your java sensei?": "miss ope" , "whats the building you are in called?":"village", "what is the name of the guy from chad":"beny","Which planet is known as the Red Planet?":"mars","Who painted the Mona Lisa?":"leonardo","Which element has the chemical symbol 'O'?":"oxygen","Which country invented paper?":"china"}
 
 count = 0
 
 def questioner():
+	
 	return random.choice(questions)
+	
 
 def question_index(num):
 	return questions.index(num)
 
 def answer(num):
-	return (answers[question_index(num)])
+	return ( random.choice(answers),random.choice(answers),random.choice(answers),
+answers[question_index(num)])
 
 def validation(input,question,count):
-	print(question)
+	
 	if input == validate_answer[question]:
-		count+=1
+		
 		return "CORRECT!"
 	else:
 		return "INCORRECT!"
--- daythree/quiz_main.py
+++ daythree/quiz_main.py
@@ -1,19 +1,25 @@
 from quiz_function import *
+count=0
 while True:
 	print(main_menu())
 	menu_input = input("ENTER INPUT: ")
 	match menu_input:
 		case "1":
-			question = questioner()
-			print(f" QUESTION ==> {question}")
-			question_index(question)
-			print(answer(question))
-			answer = input("ANSWER==>: ")
-			print(validation(answer,question,count))
-			
+			counter = 1
+			while counter <= 4:
+				question = questioner()
+				print(f" QUESTION ==> {question}")
+				question_index(question)
+				print(answer(question))
+				option = input("ANSWER==>: ").lower()
+				print(validation(option,question,count))
+				if validation(option,question,count)=="CORRECT!":
+					count+=1
+				if counter==4:
+					print(f"GAME OVER!, YOU SCORE {get_counter(count)} PLAYED {counter}")
+				counter+=1
 		case "2":
+			print("THANKS FOR PLAYING!")
 			break
-			
-			
 		case _:
 			print("ENTER A VALID INPUT")
