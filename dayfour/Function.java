c7498c1b2a0a4f6cb18403a15539bf2edd01c6b3
adedotun <erik.edwards.jon@gmail.com>
2025-10-30 14:54:38 -0700

    updATE

--- dayfour/Function.java
+++ dayfour/Function.java
@@ -1,30 +1,40 @@
 public class Function{
-
 	public static Boolean[] get_perfect_square(){
-	int [] test_list={4,6,25,49,100};
-	Boolean [] array = new Boolean [4];
-        for (int count = 0; count < test_list.length; count++) {
-		
-            int number = test_list[count];
-             	int divisor = 2;
-		
-		
-                while(number % divisor != 0) {
-                    	divisor++;
+	int [] test_list={81,6,25,49,101,7};
+	Boolean [] array = new Boolean [test_list.length];
 
-                }
-		int dividedIndex = number / divisor;
-		
-               	if (number == dividedIndex * dividedIndex) {
-                	array[count] = true;
-            	}else{
-                	array[count] = false;
-            	}
+        for (int count = 0; count < test_list.length; count++){
+		int counter = 2; 
+		while(counter !=0){
+			int value = test_list[count]%counter;
+			if (value == 0){
+				int newvalue=test_list[count]/counter;
+				if(newvalue*newvalue == test_list[count]){
+					array[count]=true;
+				}
+				else{
+					array[count]=false;
+				}
+				if(test_list[count] <=1){
+					array[count]=false;
+				}
+				counter = value;
+			}
+			else{
+				counter++;
+			}
 		}
-		
-		return array;
 	}
-
+	return array;
 }
+}
+	
+
+
+		
+
+
+
+
 
  
