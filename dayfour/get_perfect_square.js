fb56fbd7dfb62a35b2f40b9b5ec38d22b69717ec
adedotun <erik.edwards.jon@gmail.com>
2025-10-30 15:14:35 -0700

    javascript

--- dayfour/get_perfect_square.js
+++ dayfour/get_perfect_square.js
@@ -1,22 +1,31 @@
 function get_perfect_square(test_list){
 	
 	for (let count = 0; count < test_list.length; count++) {
-            let number = test_list[count];
-             	let divisor = 2;
-
-                while(number % divisor != 0) {
-                    	divisor++;
-
-                }
-		let dividedIndex = number / divisor;
-		
-               	if (number == dividedIndex * dividedIndex) {
-                	test_list[count] = true;
-            	}else{
-                	test_list[count] = false;
-            	}
+		let counter = 2;
+		if(test_list[count] <=1){
+			test_list[count]=false;
+			counter=0;
+				}
+		 
+		while(counter !=0){
+			let value = test_list[count]%counter;
+			if (value == 0){
+				let newvalue=test_list[count]/counter;
+				if(newvalue*newvalue == test_list[count]){
+					console.log(test_list[count])
+					test_list[count]=true;
+				}
+				else{
+					test_list[count]=false;
+				}
+				
+				counter = value;
+			}
+			else{
+				counter++;
+			}
 		}
-		
+	}
 return test_list;
 }
-console.log(get_perfect_square([4,9,25,49]))
+console.log(get_perfect_square([0,4,9,25,49,100,101]))
